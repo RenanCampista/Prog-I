d120b8f8dceffd4ace694889426ada19cae72d4e
RenanCampista <113597566+RenanCampista@users.noreply.github.com>
2023-03-05 14:50:01 -0300

    Add files via upload

---
+++ 1º Periodo/Boca/L1/L1_8/L1_8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    char letra;
+    scanf("%c", &letra);
+    if(letra == 'a' || letra == 'A' || letra == 'e' || letra =='E' || letra =='i' || letra =='I' || letra == 'o' || letra == 'O' || letra == 'u' || letra == 'U'){
+        printf("Vogal");
+    }else{
+        printf("Nao vogal");
+    }
+    return 0;
+}
