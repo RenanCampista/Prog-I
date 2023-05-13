e7a8f180b7c891af4e593eee9ff81ac935322e45
Renan Campista <rennan.campistah@gmail.com>
2023-05-12 21:05:55 -0300

    commit

--- 1º Periodo/Boca/L1/L1_3/L1_3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float x1, y1, x2, y2;
-    scanf("%f%f%f%f", &x1, &y1, &x2, &y2);
-    printf("%.2f", 2*(x2 - x1) + 2*(y2 - y1));
-    return 0;
-}
--- L1/L1_2/L1_2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float a, l;
-    scanf("%f%f", &a, &l);
-    printf("%.2f", 2*a + 2*l);
-    return 0;
-}
